5ad953a5d25660edbd2c3a7bea9e8cea9551dfe2
WHU-YH-jx <1092949763@qq.com>
2018-12-24 18:42:28 +0800

    New

--- Crawler/Spiders/MainProgram.cs
+++ Crawler/Spiders/MainProgram.cs
@@ -10,7 +10,7 @@ namespace Spider_HUPU
         static void Main(string[] args)
         {
             Spider_HUPU_HOSTPOST hOT = new Spider_HUPU_HOSTPOST();
-            hOT.GetFunc(new Uri("https://my.hupu.com/search?q=%E6%B8%B8&fid=&type=undefined&sortby=undefined")).Wait();
+            hOT.GetFunc(new Uri("https://my.hupu.com/search?q=%E7%A0%94%E7%A9%B6%E7%94%9F")).Wait();
             //foreach (var i in hOT.list)
             //{
             //    System.Console.WriteLine(i.ToString());
