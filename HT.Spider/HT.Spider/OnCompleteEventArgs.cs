61a956aa9241509a45efa6c34f07d3cae74d53a6
Li Peihao <1120921822@qq.com>
2018-12-28 18:45:31 +0800

    重新添加htspider源码

---
+++ HT.Spider/HT.Spider/OnCompleteEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT.Spider
+{
+    /// <summary>
+    /// OnCompleted事件的参数
+    /// </summary>
+    public class OnCompletedEventArgs
+    {
+        public Uri Uri { get; set; }
+        public int ThreadId { get; private set; }
+        public string PageSource { get; private set; }
+        public long Milliseconds { get; private set; }
+        public OnCompletedEventArgs(Uri uri, int threadId, long milliseconds, string pageSource)
+        {
+            Uri = uri;
+            ThreadId = threadId;
+            PageSource = pageSource;
+            Milliseconds = milliseconds;
+        }
+    }
+}
